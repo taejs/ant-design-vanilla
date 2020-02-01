9a9aacea7f877ec0377aae488592ce7c78e1a8c0
tae <taeshindev@gmail.com>
2020-02-01 23:19:59 +0900

    feat : create antd-progress component

---
+++ packages/antd-progress/README.md
@@ -0,0 +1,5 @@
+<div class="ant-progress">
+    <div class="ant-progress__outer">
+        <div class="ant-progress__inner"></div>
+    </div>
+</div>
---
+++ packages/antd-progress/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = [{
+  entry: './index.scss',
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ]
+      }
+    ]
+  },
+}];
